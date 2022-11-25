46efcd6c4b36c8e4727b8cea2ad552c95cf48655
henry <1151071+HenrYxZ@users.noreply.github.com>
2022-11-25 17:22:32 -0300

    wip: use wind direction vector

--- shaders/grass.glsl
+++ shaders/grass.glsl
@@ -21,9 +21,10 @@ void main()
     float t_noise = (-in_offset.z) / (grid_size * 4);
     float local_wind_factor = texture(noise, vec2(s_noise, t_noise)).x;
     float wind_drag = (
-        0.2 * sin(time + 6 * s_noise) * in_texcoord.t * local_wind_factor
+        0.2 * sin(time + 3.14 * 10 * s_noise) * in_texcoord.t * local_wind_factor
     );
-    vec3 pos = in_position * scale + in_offset + vec3(wind_drag, 0, 0);
+    vec3 wind_dir = vec3(1, 0, 0);
+    vec3 pos = in_position * scale + in_offset + wind_drag * wind_dir;
     gl_Position = m_proj * m_cam * vec4(pos, 1.0);
 }
 
